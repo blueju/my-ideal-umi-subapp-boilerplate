a13cc6346592e1bae8a9f1108789a395078237fd
blueju <blueju@foxmail.com>
2021-05-03 21:47:44 +0800

    更新首页

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>https://github.com/blueju/my-ideal-umi-subapp-boilerplate</h1>
     </div>
   );
 }
