71d0c8ba26d7c746bab830ef0f3416a78c098291
blueju <blueju@foxmail.com>
2021-05-03 22:18:27 +0800

    完善开发环境临时菜单的路由配置

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,9 +1,22 @@
+/**
+ * 路由配置，
+ * 强烈建议将 name，title 也写上，
+ * name 写上可自动生产开发环境临时菜单，
+ * title 写上可自动切换标签页名称。
+ * @type [{name:string,title:string,path:string,component:string}]
+ */
 const routes = [
   {
     path: '/',
     component: '../layouts/BasicLayout',
+    flatMenu: true,
     routes: [
-      { path: '/', component: '@/pages/index' },
+      {
+        name: 'Index',
+        title: 'Index',
+        path: '/',
+        component: '@/pages/index',
+      },
       {
         component: './404',
       },
