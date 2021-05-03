e86a067686a55e37c3461b31d32a46219a91327e
blueju <blueju@foxmail.com>
2021-05-03 21:45:30 +0800

    启用基本布局，启用 404 页面

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,2 +1,16 @@
-const routes = [{ path: '/', component: '@/pages/index' }];
+const routes = [
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    routes: [
+      { path: '/', component: '@/pages/index' },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
 export default routes;
