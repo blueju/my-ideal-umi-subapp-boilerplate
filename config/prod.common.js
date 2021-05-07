4aafdcf70f12e1b326bfbda481cdbf453f7b9272
blueju <blueju@foxmail.com>
2021-05-07 21:16:48 +0800

    让生成的文件包含 hash 后缀 配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存。

--- config/prod.common.js
+++ config/prod.common.js
@@ -14,6 +14,15 @@ function getOutputPath() {
 
 /** 生产公共配置 */
 const prodCommonConfig = {
+  /**
+   * 启用 hash，
+   * 参考：https://umijs.org/zh-CN/config#hash
+   */
+  hash: true,
+  /**
+   * 打算打包时才去适配 ie11, 减少开发环境的编译时间，
+   * 如果你需要在开发环境中测试在 ie11 下的兼容性，请将此条移至 config.js
+   **/
   targets: { ie: 11 },
   /** 配置额外的 babel 插件 */
   extraBabelPlugins: [
